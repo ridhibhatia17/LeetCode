9a0a0ed867f3ccb6879ad63c73d9cd92057b291f
ridhi bhatia <ridhbht4@gmail.com>
2026-02-11 15:00:25 +0530

    Solved leetcode problem 1438

---
+++ SlidingWindow/LongestContinuousSubarrayWithAbsDifference(1438).java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestSubarray(int[] nums,int limit){
+        Deque<Integer> maxDeque=new ArrayDeque<>();
+        Deque<Integer> minDeque=new ArrayDeque<>();
+        int left=0,res=0;
+        for(int right=0;right<nums.length;right++){
+            while(!maxDeque.isEmpty()&&nums[right]>maxDeque.peekLast())maxDeque.pollLast();
+            maxDeque.addLast(nums[right]);
+            while(!minDeque.isEmpty()&&nums[right]<minDeque.peekLast())minDeque.pollLast();
+            minDeque.addLast(nums[right]);
+            while(maxDeque.peekFirst()-minDeque.peekFirst()>limit){
+                if(nums[left]==maxDeque.peekFirst())maxDeque.pollFirst();
+                if(nums[left]==minDeque.peekFirst())minDeque.pollFirst();
+                left++;
+            }
+            res=Math.max(res,right-left+1);
+        }
+        return res;
+    }
+}
