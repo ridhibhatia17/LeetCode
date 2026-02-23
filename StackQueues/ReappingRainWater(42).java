2f24dab70548e15a3cc3038e3c8b4c2c02d240ca
ridhi bhatia <ridhbht4@gmail.com>
2026-02-23 11:13:54 +0530

    Solved leetcode problem 42

---
+++ StackQueues/ReappingRainWater(42).java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int left=0,right=n-1,leftmax=0,rightmax=0,res=0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }else{
+                    res+=leftmax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }else{
+                    res+=rightmax-height[right];
+                }
+                right--;
+            }    
+        }
+        return res;
+    }
+}
