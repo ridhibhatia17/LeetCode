4b98977d2d8a1ae2297f3313f3d071ab6e8cea75
ridhi bhatia <ridhbht4@gmail.com>
2026-03-05 14:46:16 +0530

    Solved leetcode problem 1758

---
+++ Strings/MinimumChangesToMakeAlternatingBinaryString(1758).java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minOperations(String s) {
+        int count = 0, n = s.length();
+        for (int i = 0; i < n; i++)
+            count += (s.charAt(i) ^ i) & 1
+        return Math.min(count, n - count);
+    }
+}
