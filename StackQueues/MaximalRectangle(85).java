850059873b7fe48231a504a8f58e25e5f5bd7f4e
ridhi bhatia <ridhbht4@gmail.com>
2026-03-09 09:56:21 +0530

    Solved leetcode problem 85

---
+++ StackQueues/MaximalRectangle(85).java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+        return maxArea;
+    }
+    private int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+                int mid = stack.pop();
+                int height = heights[mid];
+                int right = i;
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int width = right - left - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
